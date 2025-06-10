9863749ee6df986fc4258fa3ce84c05071370c49
Alisson Pereira <alissonpef@gmail.com>
2025-06-10 12:01:46 -0300

    Starting the Backend course

--- Fundamentals_of_Nodejs/1-Event_loop/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("node:fs")
-console.log("Start")
-
-fs.readFile("./data.txt", "utf-8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-})
-
-console.log("End")
--- Fundamentals_of_Nodejs/2-Modules_nodejs/main.js
+++
@@ -1,8 +0,0 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url); // Create the require because we use module as the default in package.sjon
-
-const sum = require("./sum.cjs");
-import sub from "./subtract.mjs";
-
-console.log(sum(2, 2));
-console.log(sub(2, 2));
