9abf382fbfc6e4951b9a9ee47a41e5f7e0049e84
Alisson Pereira <alissonpef@gmail.com>
2025-06-14 15:30:55 -0300

    feat(Node.js): Add studies on the Path module

--- Fundamentals_of_Nodejs/4-Path/index.js
+++ Fundamentals_of_Nodejs/4-Path/index.js
@@ -3,13 +3,17 @@ const path = require("node:path")
 const dir = "src"
 const file = "app.js"
 
-const fullPath = path.join(dir, file)
+const fullPath = path.join(__dirname, dir, file) // __dirname pega o caminho absoluto do diretório atual e join combina com os outros segmentos
 console.log(fullPath)
 
-const relativePath = "../arquivos/relatorio.pdf"
+const relativePath = path.join(".", dir, file) // "." Representa o diretório atual
+console.log(relativePath)
 
-const absolutePath = path.resolve(relativePath)
-console.log(absolutePath)
+const absolutePath = path.resolve(__dirname, relativePath) // Resolve converte o caminho relativo em absoluto
+console.log(absolutePath) 
 
-const fileName = path.basename(relativePath)
+const fileName = path.basename(relativePath) // basename extrai o nome do arquivo do caminho
 console.log(fileName)
+
+const ext = path.extname(absolutePath) // extname extrai a extensão do arquivo do caminho absoluto
+console.log(ext)
