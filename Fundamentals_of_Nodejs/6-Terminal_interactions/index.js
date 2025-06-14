5ddb0b7897ca03f476f09eab16601d6bbedba81c
Alisson Pereira <alissonpef@gmail.com>
2025-06-14 17:38:08 -0300

    feat(Node.js): Add studies on the Terminal Interactions

---
+++ Fundamentals_of_Nodejs/6-Terminal_interactions/index.js
@@ -0,0 +1,36 @@
+const readline = require("node:readline")
+
+// process.stdout.write("Olá, mundo!\n")
+
+// process.stdin.on("data", (data) => {
+//   process.stdout.write(`Você digitou: ${data}`)
+// })
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+// Ele escreve além do que nós digitamos, ele escreva tambem Você digitou: "texto"
+rl.on("line", (input) => {
+  rl.write(`Você digitou: "${input}"`)
+})
+
+rl.question("Qual é o seu nome? ", (answer) => {
+  rl.write(`Olá, ${answer}!\\n`)
+  rl.close()
+})
+
+rl.on("close", () => {
+  rl.write("Saindo...")
+  // repare que ao adicionar um listener para "close" precisaremos
+	// encerrar o processo atual manualmente ou ele ficará "pendurado"
+	process.exit(0)
+})
+
+rl.on('SIGINT', () => {
+  rl.question('Deseja realmente sair? (s/n) ', (resposta) => {
+    if (resposta.trim().toLowerCase() === 's') {
+      rl.close()
+    } else {
+      rl.write("Você escolheu continuar.")
+    }
+  })
+})
