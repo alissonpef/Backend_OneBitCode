7177088a0315003b58ff8b2af909530789607939
Alisson Pereira <alissonpef@gmail.com>
2025-07-02 11:10:21 -0300

    feat(exercises): Add exercise 3

--- Web_Applications/5-Post_and_Redirections/src/index.js
+++ Web_Applications/5-Post_and_Redirections/src/index.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 // Configuração do  Body
 app.use(express.urlencoded({ extended: true }));
-
+  
 app.get("/", (req, res) => {
   const tittle = "Homepage";
   const message = "Mensagem dinâmica inserida pelo EJS";
