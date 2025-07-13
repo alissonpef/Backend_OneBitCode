495747922625225e3f564399453d30370be18f34
Alisson Pereira <alissonpef@gmail.com>
2025-07-13 16:53:44 -0300

    feat(Node.js): Add Post and Redirections files Project

--- Web_Applications/5-Post_and_Redirections/src/index.js
+++ Web_Applications/5-Post_and_Redirections/src/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const path = require("node:path");
 const { title } = require("node:process");
-  
+
 const app = express();
 const storedUsers = [];
-  
+
 // Configuração do EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-  
+
 // Configuração do  Body
 app.use(express.urlencoded({ extended: true }));
-  
+
 app.get("/", (req, res) => {
   const tittle = "Homepage";
   const message = "Mensagem dinâmica inserida pelo EJS";
@@ -22,21 +22,20 @@ app.get("/", (req, res) => {
 app.get("/formulario", (req, res) => {
   res.render("forms");
 });
-  
+
 app.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  
+
   storedUsers.push({ username, password });
   console.log(req.body);
-  
+
   res.redirect("/usuarios");
 });
 
 app.get("/usuarios", (req, res) => {
   res.render("users", { users: storedUsers });
 });
-  
+
 const PORT = 3000;
 app.listen(PORT, () => console.log("Servidor iniciado!"));
-  
