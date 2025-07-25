8fe73c594527ceb55bd694b0a5252e7075cf7e26
Alisson Pereira <alissonpef@gmail.com>
2025-07-25 12:43:13 -0300

    feat(Node.js): Adding a folder with files about Express

---
+++ Web_Applications/2-Express/src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Servidor Express funcionando!\nVocê está na página inicial.");
+});
+server.get("/artigos", (req, res) => {
+  res.send("Você está na página artigos.");
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Servidor Express iniciado em http://localhost:${PORT}/`);
+});
