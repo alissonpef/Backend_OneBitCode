697b6a5d28652bf7b59def0e38668fdd5fb24c90
Alisson Pereira <alissonpef@gmail.com>
2025-07-28 18:02:52 -0300

    feat(Node.js): Adding a folder with files about Returning Json

---
+++ Web_Applications/7-Returning_Json/src/routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const gamesController = require('./controllers/games-controller')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+	res.json({ message: 'Hello, world!' })
+})
+
+router.get('/games', gamesController.index)
+router.get('/games/:id', gamesController.show)
+router.post('/games', gamesController.save)
+router.post('/games/:id/genres', gamesController.addGenre)
+
+module.exports = router
